67d4853acea37b494bf90a03d769232b6373cf42
Rotaryz <1016994190@qq.com>
2017-08-08 19:27:11 +0800

    完成首页

--- src/js/directive.js
+++ src/js/directive.js
@@ -1,60 +1,65 @@
 //组件
-;(function(){
-	// 头部
-	var directive = angular.module('directive',[]);
-	directive.directive("jheader",["$http",function($http){
- 		return	{
- 			templateUrl:"directive/jheader.html",
- 			link:function(scope,ele,attr){
- 				scope.right_btn = "删除";
- 				scope.title = "购物车";
-				scope.isMaskShow = false;
- 				scope.headerClick = function(){
- 					scope.isMaskShow = true;
- 				}
- 			}
- 		}
-	}])
-	// 底部
-	directive.directive("jfooter",[function(){
- 		return	{
- 			templateUrl:"directive/jfooter.html",
- 			link:function(scope,ele,attr){
- 				scope.showfoot = function(idx){
- 					scope.isshowfoot = idx;
- 				}
- 			}
- 		}
-	}])
-	// 遮罩层
-	directive.directive("jmask",[function(){
- 		return	{
- 			templateUrl:"directive/jmask.html",
- 			link:function(scope,ele,attr){
- 				scope.isSure = function(){
- 					scope.isMaskShow = false;
- 				}
- 				scope.isCancel = function(){
- 					scope.isMaskShow = false;
- 				}
- 			}
- 		}
-	}])
+;
+(function() {
+    // 头部
+    var directive = angular.module('directive', []);
+    directive.directive("jheader", ["$http", function($http) {
+        return {
+            templateUrl: "directive/jheader.html",
+            link: function(scope, ele, attr) {
+                scope.right_btn = "删除";
+                scope.title = "购物车";
+                scope.isMaskShow = false;
+                scope.headerClick = function() {
+                    scope.isMaskShow = true;
+                }
+            }
+        }
+    }])
+    // 底部
+    directive.directive("jfooter", [function() {
+        return {
+            templateUrl: "directive/jfooter.html",
+            link: function(scope, ele, attr) {
+                scope.showfoot = function(idx) {
+                    scope.isshowfoot = idx;
+                }
+            }
+        }
+    }])
+    // 遮罩层
+    directive.directive("jmask", [function() {
+        return {
+            templateUrl: "directive/jmask.html",
+            link: function(scope, ele, attr) {
+                scope.isSure = function() {
+                    scope.isMaskShow = false;
+                }
+                scope.isCancel = function() {
+                    scope.isMaskShow = false;
+                }
+            }
+        }
+    }])
 
-    directive.directive('xheader', ['$http', function($http) {
+    directive.directive('xheader', ['$http', '$state', function($http, $state) {
         return {
             templateUrl: 'directive/xheader.html',
             link: function(scope, ele, attr) {
                 scope.text = '';
                 scope.title = '';
-                scope.h1 = 'javascript:;';
-                scope.baseUrl = './data/'
+                scope.xh1 = 'javascript:;';
+                scope.baseUrl = './data/';
+                scope.isshowtsea = true;
+                scope.isshowsearch = false;
                 scope.showSea = function() {
                     scope.isshowsearch = true;
                     scope.title = '取消';
                 };
                 scope.showtsea = function() {
+                    // console.log('dfs')
                     if (scope.text.length > 0) {
+
                         scope.isshowtsea = false;
                         scope.title = '搜索';
                         scope.seekmsg()
@@ -67,37 +72,39 @@
                 scope.hidetsea = function() {
                     scope.text = '';
                     scope.isshowtsea = true;
+                    ele.children().children().children().eq(1)[0].focus();
                 };
                 scope.act = function() {
                     if (scope.title == '取消') {
                         scope.isshowsearch = false;
                     } else if (scope.title == '搜索') {
-                        scope.h1 = '#/index/filist/pseek/pop/1/' + scope.text;
+                        scope.xh1 = '#!/index/filist/pseek/pop/' + scope.text;
                         scope.getfilist('pop')
                     }
                 };
-                scope.list = [];
+                scope.searchlist = [];
                 scope.seekmsg = function() {
                     $http({
                         url: scope.baseUrl + 'goodlist.json',
                         method: 'get',
 
                     }).then(function(data) {
-                        var all = data.data.RECORDS
-                        var arr = []
-                        console.log(scope.text)
-                        for (var i = 0; i < all.length; i++) {
-                            if (all[i].title.includes(scope.text)) {
-                                // console.log(all[i])
-                                arr.push(all[i])
+                        scope.all = data.data.RECORDS
+                        scope.arr = []
+                        for (var i = 0; i < scope.all.length; i++) {
+                            if (scope.all[i].title.includes(scope.text)) {
+                                // console.log(scope.all[i])
+                                scope.arr.push(scope.all[i])
                             }
                         }
-                        if (arr.length <= 10) {
-                            scope.list = data.data.list;
-                            // console.log(scope.searchlist)
+                        if (scope.arr.length <= 10) {
+                            scope.searchlist = scope.arr;
+
                         } else {
-                            scope.searchlist = arr.slice(0, 10);
+                            scope.searchlist = scope.arr.slice(0, 10);
                         }
+                        // console.log(scope.searchlist)
+
                     }).then(function(err) {
 
                     })
@@ -110,14 +117,14 @@
                         method: 'get',
 
                     }).then(function(data) {
-                        var all = data.data.RECORDS
-                        var arr = []
-                        for (var i = 0; i < all.length; i++) {
-                            if (all[i].sort == this.$route.params.sort) {
-                                arr.push(all[i])
+                        scope.all = data.data.RECORDS
+                        scope.arr = []
+                        for (var i = 0; i < scope.all.length; i++) {
+                            if (scope.all[i].sort == $state.sort) {
+                                scope.arr.push(scope.all[i])
                             }
                         }
-                        scope.list = scope.list.concat(arr.slice(0, 10));
+                        scope.list = scope.list.concat(scope.arr.slice(0, 10));
                     }).then(function(err) {
 
                     })
@@ -168,7 +175,7 @@
                 scope.getActive = function() {
                     $http({
                         method: 'get',
-                        url: './data/active.json',
+                        url: scope.baseUrl + 'active.json',
                     }).then(function(data) {
                         scope.special = data.data.flist.slice(0, 4);
                         scope.liactive = data.data.olist;
@@ -276,6 +283,7 @@
                     scope.target = scope.tar.eq(i).children().eq(1).children();
                     scope.target[0].style.width = 1360 + 'px'
                     scope.target.on('touchstart', function(data1, eve) {
+
                         scope.start = data1.changedTouches['0'].pageX;
                         scope.left = this.offsetLeft;
                         scope.self = this;
@@ -297,46 +305,138 @@
         }
     }]);
 
-    directive.directive('xlist', ['$http','$state', function($http,$state) {
+    directive.directive('xlist', ['$http', '$state', function($http, $state) {
         return {
             templateUrl: 'directive/xlist.html',
             link: function(scope, ele, attr) {
-                scope.onTop = function() {
-                    window.onscroll = function() {
-                        if (window.scrollY >= 2490) {
-                            this.isontop = true;
-                        } else {
-                            this.isontop = false;
-                        }
-                    }
-                };
                 scope.isshownav = 0;
-                scope.onTop();
-                scope.shownav = function(num, sort) {
+                scope.isshowmore = false;
+                // console.log($state.params)
+                scope.shownav = function(num) {
+                    scope.plist = [];
                     scope.isshownav = num;
+                    scope.getlist(1);
+
+                };
+                scope.getlist = function(page) {
+                    scope.isshowmore = true;
                     $http({
                         method: 'get',
-                        url: './data/goodlist.json',
+                        url: scope.baseUrl +
+                            'goodlist.json',
                     }).then(function(data) {
                         scope.all = data.data.RECORDS;
                         scope.arr = []
-                        for (var i = 0; i < scope.all.length; i++) {
-                            if (scope.all[i].sort == $state.params.sort) {
-                                scope.arr.push(scope.all[i])
+                        if (attr.type == 'home') {
+                            scope.h1 = '#!/index/home/list/pop';
+                            scope.h2 = '#!/index/home/list/sell';
+                            scope.h3 = '#!/index/home/list/new';
+                            for (var i = 0; i < scope.all.length; i++) {
+                                if (scope.all[i].sort == $state.params.sort) {
+                                    scope.arr.push(scope.all[i])
+                                }
+                            };
+                            window.onscroll = function() {
+                                if (window.scrollY >= 2490) {
+                                    scope.isontop = true;
+                                } else {
+                                    scope.isontop = false;
+                                }
                             }
-                        };
-                        scope.list = scope.arr.slice(0,10);
-                        console.log(scope.list)
-                    })
+
+                        } else if (attr.type == 'sou') {
+                            scope.h1 = '#!/index/filist/pseek/pop/' + $state.params.title;
+                            scope.h2 = '#!/index/filist/pseek/sell/' + $state.params.title;
+                            scope.h3 = '#!/index/filist/pseek/new/' + $state.params.title;
+                            for (var i = 0; i < scope.all.length; i++) {
+                                if (scope.all[i].sort == $state.params.sort && scope.all[i].title.includes($state.params.title)) {
+                                    // console.log()
+                                    scope.arr.push(scope.all[i])
+                                }
+                            };
+
+                        }
+                        scope.start = page * 10;
+                        scope.end = scope.start + 10;
+                        // scope.plist = scope.arr.slice(0, 10);
+                        scope.isshowmore = false;
+                        if (scope.end < scope.arr.length) {
+                            scope.plist = scope.plist.concat(scope.arr.slice(scope.start, scope.end))
+                        } else {
+                            scope.wasmore = false;
+                            scope.isnone = true
+                        }
+                    });
                 };
-                if($state.params.sort == 'pop'){
+
+
+                if ($state.params.sort == 'pop') {
                     scope.isshownav = 0;
-                }else if($state.params.sort == 'sell'){
+                } else if ($state.params.sort == 'sell') {
                     scope.isshownav = 1;
-                }else if($state.params.sort == 'new'){
+                } else if ($state.params.sort == 'new') {
                     scope.isshownav = 2;
                 }
-                scope.shownav(scope.isshownav)
+                scope.shownav(scope.isshownav);
+
+            }
+        }
+    }]);
+
+    directive.directive('xclist', [function() {
+        return {
+            templateUrl: './template/route/xclist.html',
+            link: function(scope, ele, attr) {
+                // console.log(attr, ele)
+            }
+        }
+    }]);
+
+    directive.directive('xfooter', ['$state', function($state) {
+        return {
+            templateUrl: './directive/xfooter.html',
+            link: function(scope, ele, attr) {
+                scope.isshowfoot = 1;
+                scope.showfoot = function(num) {
+                    if (num == 1) {
+                        scope.isshowsearch = false;
+                    }
+                    scope.isshowfoot = num;
+                    // if (num == 1) {
+                    //     scope = false
+                    // }
+                    // console.log(this.isshowfoot)
+                };
+                if ($state.current.url == '/list/:sort') {
+                    scope.isshowfoot = 1;
+
+                } else if ($state.current.url == '/category') {
+                    scope.isshowfoot = 2;
+                } else if ($state.current.url == '/car') {
+                    scope.isshowfoot = 3;
+                } else if ($state.current.url == '/mycenter') {
+                    scope.isshowfoot = 4;
+                }
+
+            }
+        }
+    }]);
+
+    directive.directive('xsearch', [function() {
+        return {
+            templateUrl: './directive/xsearch.html',
+            link: function(scope, ele, attr) {
+                scope.isnone = false;
+                scope.page = 1;
+                window.onscroll = function() {
+                    // console.log(document.body.offsetHeight - window.scrollY)
+                    if (document.body.offsetHeight - window.scrollY == 587) {
+                        scope.page++;
+                        console.log(scope.page)
+                        scope.getlist(scope.page);
+                    }
+
+                }
             }
         }
     }])
--- src/js/filter.js
+++ src/js/filter.js
@@ -1,4 +1,10 @@
 //指令
 ;(function(){
-	var service = angular.module('filter',[]);
+	var filter = angular.module('filter',[]);
+	// 显示是个字
+	filter.filter('ten',function(){
+		return function(data){
+			return data.slice(0,10)
+		}
+	})
 })();
